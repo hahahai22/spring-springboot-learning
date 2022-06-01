ab5c2faac64dc9ac395b7e29f9e4938251cb8ffe
焦海龙 <jiaohailong@jiaohailongdeMacBook-Pro.local>
2022-06-01 09:44:02 +0800

    sm

--- spring-010-sm/spring-010-sm/src/main/java/com/bjpowernode/service/impl/AccountsServiceImpl.java
+++ spring-010-sm/spring-010-sm/src/main/java/com/bjpowernode/service/impl/AccountsServiceImpl.java
@@ -1,9 +1,12 @@
 package com.bjpowernode.service.impl;
 
+import com.bjpowernode.mapper.AccountsMapper;
 import com.bjpowernode.pojo.Accounts;
 import com.bjpowernode.service.AccountsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @author 焦海龙
@@ -11,14 +14,28 @@ import org.springframework.stereotype.Service;
  * @date 2022/5/29 15:03
  */
 @Service  // 交给Spring容器去创建对象
+// 这里的propagation = Propagation.REQUIRED是事务的传播特性，增删改必须走这个特性。
+// 指定发生什么异常事务不回滚。noRollbackForClassName使用的是异常的名称。
+// noRollbackFor，使用的是异常的类型
+// rollbackForClassName指定发生什么异常，必须回滚
+// rollbackFor 指定发生什么异常类型，必须回滚
+// timeOut=-1 默认是-1，表示永不超时
+// readOnly 默认是false增删改操作，如果是查询操作，设置为true
+// isolation=Isolation.DEFAULT 使用数据库自己默认的隔离级别 "数据库有四种隔离级别：读未提交、读已提交、可重复度、串行/序列化读"
+//@Transactional(propagation = Propagation.REQUIRED, noRollbackForClassName = "ArithmeticException")
 public class AccountsServiceImpl implements AccountsService {
 
     // 切记：逻辑访问层必须有数据访问层的对象
     @Autowired      // 交给Spring依赖自动注入（值）
-    AccountsService accountsService;
+    AccountsMapper accountsMapper;
 
     @Override
     public int save(Accounts accounts) {
+        int num = 0;
+        num = accountsMapper.save(accounts);
+        System.out.println("账户增加成功num!：" + num);
+        // 手动抛出异常
+        System.out.println(1/0);
         return 0;
     }
 }
--- spring-010-sm/spring-010-sm/src/test/java/com/bjpowernode/test/MyTest.java
+++ spring-010-sm/spring-010-sm/src/test/java/com/bjpowernode/test/MyTest.java
@@ -1,6 +1,8 @@
 package com.bjpowernode.test;
 
+import com.bjpowernode.pojo.Accounts;
 import com.bjpowernode.pojo.Users;
+import com.bjpowernode.service.AccountsService;
 import com.bjpowernode.service.UsersService;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
@@ -16,9 +18,22 @@ public class MyTest {
     public void Test01() {
         // 创建容器对象并启动
         ApplicationContext ac = new ClassPathXmlApplicationContext("applicationContext_service.xml");
-        // 取出UserServiceImpl
+        // 取出UserServiceImpl对象
         UsersService uservice = (UsersService) ac.getBean("userServiceImpl");
         int num = uservice.insert(new Users(100, "zhangsan", "123"));
         System.out.println(num);
+    }
+
+    @Test
+    public void Test02() {
+        // 创建容器对象并启动
+        ApplicationContext ac = new ClassPathXmlApplicationContext("applicationContext_service.xml");
+        // 取出AccountService对象
+        AccountsService accountsService = (AccountsService) ac.getBean("accountsServiceImpl");
+        System.out.println(accountsService.getClass());
+        // 传入实体类对象
+        accountsService.save(new Accounts(204, "李六2", "账户安全04"));
+
+
     }
 }
